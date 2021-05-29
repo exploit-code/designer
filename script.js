4fc3c5e7d66b5869184b8ac816d0c8dd56956648
Alexander <o_xx@list.ru>
2021-05-29 19:46:11 +0300

    header position sticky

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-const link = document.querySelectorAll('.header-nav-list__item');
-const box = document.querySelectorAll('.box');
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-for (let x = 0; x < link.length; x++) {
-    for (let y = 0; y < box.length; y++) {
-        link[x++].addEventListener('click', (evt) => {
-            evt.preventDefault();
-            box[y].scrollIntoView({
-                behavior: "smooth"
-            });
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
         });
-    }
-}
+    });
+});
